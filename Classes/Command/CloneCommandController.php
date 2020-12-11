b2fcd70e7d4cb32c9beb399c81ca33aec26fd285
Christian Schwahn <christian.schwahn@di-unternehmer.com>
2020-12-11 15:52:06 +0100

    TASK(aws): add aws dump command

--- Classes/Command/CloneCommandController.php
+++ Classes/Command/CloneCommandController.php
@@ -1,22 +1,25 @@
 <?php
 namespace Sitegeist\MagicWand\Command;
 
-/*                                                                        *
- * This script belongs to the Neos Flow package "Sitegeist.MagicWand".    *
- *                                                                        *
- *                                                                        */
-
 use Neos\Flow\Annotations as Flow;
 use Neos\Utility\Arrays;
 use Neos\Flow\Core\Bootstrap;
 use Sitegeist\MagicWand\DBAL\SimpleDBAL;
 use Symfony\Component\Yaml\Yaml;
+use Sitegeist\MagicWand\Service\LambdaService;
 
 /**
  * @Flow\Scope("singleton")
  */
 class CloneCommandController extends AbstractCommandController
 {
+
+    /**
+     * @Flow\InjectConfiguration(package="Sitegeist.MagicWand", path="aws.enabled")
+     * @var boolean
+     */
+    protected $awsEnabled;
+
 
     /**
      * @Flow\Inject
@@ -43,6 +46,12 @@ class CloneCommandController extends AbstractCommandController
     protected $dbal;
 
     /**
+     * @Flow\Inject
+     * @var LambdaService
+     */
+    protected $lambdaService;
+
+    /**
      * Show the list of predefined clone configurations
      */
     public function listCommand()
@@ -58,23 +67,6 @@ class CloneCommandController extends AbstractCommandController
             }
         }
     }
-
-    /**
-     * Clones the default preset
-     *
-     * @param boolean $yes confirm execution without further input
-     * @param boolean $keepDb skip dropping of database during sync
-     */
-    public function defaultCommand(bool $yes = false, bool $keepDb = false) : void
-    {
-        if ($this->defaultPreset === null || $this->defaultPreset === '') {
-            $this->renderLine('There is no default preset configured!');
-            $this->quit(1);
-        }
-
-        $this->presetCommand($this->defaultPreset, $yes, $keepDb);
-    }
-
     /**
      * Clone a flow setup as specified in Settings.yaml (Sitegeist.MagicWand.clonePresets ...)
      *
@@ -82,7 +74,7 @@ class CloneCommandController extends AbstractCommandController
      * @param boolean $yes confirm execution without further input
      * @param boolean $keepDb skip dropping of database during sync
      */
-    public function presetCommand($presetName, $yes = false, $keepDb = false)
+    public function dumpCommand($presetName, $yes = false, $keepDb = false)
     {
         if (count($this->clonePresets) > 0) {
             if ($this->clonePresets && array_key_exists($presetName, $this->clonePresets)) {
@@ -91,23 +83,12 @@ class CloneCommandController extends AbstractCommandController
                 $configuration = $this->configurationService->getCurrentConfiguration();
 
                 $this->renderLine('Clone by preset ' . $presetName);
-                $this->cloneRemoteHost(
-                    $configuration['host'],
-                    $configuration['user'],
-                    $configuration['port'],
-                    $configuration['path'],
-                    $configuration['context'],
+                $this->importRemoteDump(
                     (isset($configuration['postClone']) ?
                         $configuration['postClone'] : null
                     ),
                     $yes,
-                    $keepDb,
-                    (isset($configuration['flowCommand']) ?
-                        $configuration['flowCommand'] : null
-                    ),
-                    (isset($configuration['sshOptions']) ?
-                        $configuration['sshOptions'] : ''
-                    )
+                    $keepDb
                 );
             } else {
                 $this->renderLine('The preset ' . $presetName . ' was not found!');
@@ -119,66 +100,12 @@ class CloneCommandController extends AbstractCommandController
         }
     }
 
-    /**
-     * Clone a Flow Setup via detailed hostname
-     *
-     * @param string $host ssh host
-     * @param string $user ssh user
-     * @param string $port ssh port
-     * @param string $path path on the remote server
-     * @param string $context flow_context on the remote server
-     * @param mixded $postClone command or array of commands to be executed after cloning
-     * @param boolean $yes confirm execution without further input
-     * @param boolean $keepDb skip dropping of database during sync
-     * @param string $remoteFlowCommand the flow command to execute on the remote system
-     * @param string $sshOptions additional options for the ssh command
-     */
-    protected function cloneRemoteHost(
-        $host,
-        $user,
-        $port,
-        $path,
-        $context = 'Production',
+    protected function importRemoteDump(
         $postClone = null,
         $yes = false,
-        $keepDb = false,
-        $remoteFlowCommand = null,
-        $sshOptions = ''
+        $keepDb = false
     )
     {
-        // fallback
-        if ($remoteFlowCommand === null) {
-            $remoteFlowCommand = $this->flowCommand;
-        }
-
-        // read local configuration
-        $this->renderHeadLine('Read local configuration');
-
-        $localDataPersistentPath = FLOW_PATH_ROOT . 'Data/Persistent';
-
-        // read remote configuration
-        $this->renderHeadLine('Fetch remote configuration');
-        $remotePersistenceConfigurationYaml = $this->executeLocalShellCommand(
-            'ssh -p %s %s %s@%s "cd %s; FLOW_CONTEXT=%s '
-            . $remoteFlowCommand
-            . ' configuration:show --type Settings --path Neos.Flow.persistence.backendOptions;"',
-            [
-                $port,
-                $sshOptions,
-                $user,
-                $host,
-                $path,
-                $context
-            ],
-            [
-                self::HIDE_RESULT
-            ]
-        );
-
-        if ($remotePersistenceConfigurationYaml) {
-            $remotePersistenceConfiguration = \Symfony\Component\Yaml\Yaml::parse($remotePersistenceConfigurationYaml);
-        }
-        $remoteDataPersistentPath = $path . '/Data/Persistent';
 
         #################
         # Are you sure? #
@@ -200,44 +127,11 @@ class CloneCommandController extends AbstractCommandController
         ######################
         # Measure Start Time #
         ######################
-
         $startTimestamp = time();
-
-        ##################
-        # Define Secrets #
-        ##################
-
-        $this->addSecret($this->databaseConfiguration['user']);
-        $this->addSecret($this->databaseConfiguration['password']);
-        $this->addSecret(escapeshellcmd($this->databaseConfiguration['password']));
-        $this->addSecret(escapeshellarg(escapeshellcmd($this->databaseConfiguration['password'])));
-        $this->addSecret($remotePersistenceConfiguration['user']);
-        $this->addSecret($remotePersistenceConfiguration['password']);
-        $this->addSecret(escapeshellcmd($remotePersistenceConfiguration['password']));
-        $this->addSecret(escapeshellarg(escapeshellcmd($remotePersistenceConfiguration['password'])));
-
-        #######################
-        # Check Configuration #
-        #######################
-
-        $this->checkConfiguration($remotePersistenceConfiguration);
-
-        ################################################
-        # Fallback to default MySQL port if not given. #
-        ################################################
-
-        if (!isset($remotePersistenceConfiguration['port'])) {
-            $remotePersistenceConfiguration['port'] = $this->dbal->getDefaultPort($remotePersistenceConfiguration['driver']);
-        }
-
-        if (!isset($this->databaseConfiguration['port'])) {
-            $this->databaseConfiguration['port'] = $this->dbal->getDefaultPort($this->databaseConfiguration['driver']);
-        }
 
         ########################
         # Drop and Recreate DB #
         ########################
-
         if ($keepDb == false) {
             $this->renderHeadLine('Drop and Recreate DB');
 
@@ -264,23 +158,11 @@ class CloneCommandController extends AbstractCommandController
         ######################
         #  Transfer Database #
         ######################
-
         $this->renderHeadLine('Transfer Database');
+        $file = $this->lambdaService->getLambdaContent();
         $this->executeLocalShellCommand(
-            'ssh -p %s %s %s@%s -- %s | %s',
+            'cat '.$file.' | %s',
             [
-                $port,
-                $sshOptions,
-                $user,
-                $host,
-                $this->dbal->buildDumpCmd(
-                    $remotePersistenceConfiguration['driver'],
-                    $remotePersistenceConfiguration['host'],
-                    (int)$remotePersistenceConfiguration['port'],
-                    $remotePersistenceConfiguration['user'],
-                    escapeshellcmd($remotePersistenceConfiguration['password']),
-                    $remotePersistenceConfiguration['dbname']
-                ),
                 $this->dbal->buildCmd(
                     $this->databaseConfiguration['driver'],
                     $this->databaseConfiguration['host'],
@@ -292,75 +174,16 @@ class CloneCommandController extends AbstractCommandController
             ]
         );
 
-        ##################
-        # Transfer Files #
-        ##################
+        ####################################
+        # Check resourceProxyConfiguration #
+        ####################################
 
         $resourceProxyConfiguration = $this->configurationService->getCurrentConfigurationByPath('resourceProxy');
 
         if (!$resourceProxyConfiguration) {
-            $this->renderHeadLine('Transfer Files');
-            $this->executeLocalShellCommand(
-                'rsync -e "ssh -p %s %s" -kLr %s@%s:%s/* %s',
-                [
-                    $port,
-                    addslashes($sshOptions),
-                    $user,
-                    $host,
-                    $remoteDataPersistentPath,
-                    $localDataPersistentPath
-                ]
-            );
-        } else {
-            $this->renderHeadLine('Transfer Files - without Resources because a resourceProxyConfiguration is found');
-            $this->executeLocalShellCommand(
-                'rsync -e "ssh -p %s %s" --exclude "Resources/*" -kLr %s@%s:%s/* %s',
-                [
-                    $port,
-                    addslashes($sshOptions),
-                    $user,
-                    $host,
-                    $remoteDataPersistentPath,
-                    $localDataPersistentPath
-                ]
-            );
+            $this->renderHeadLine( 'resourceProxyConfiguration not found!');
         }
 
-        #########################
-        # Transfer Translations #
-        #########################
-
-        $this->renderHeadLine('Transfer Translations');
-
-        $remoteDataTranslationsPath = $path . '/Data/Translations';
-        $localDataTranslationsPath = FLOW_PATH_ROOT . 'Data/Translations';
-
-        // If the translation directory is available print true - because we didn't get the return value here
-        $translationsAvailable = trim(
-            $this->executeLocalShellCommand(
-                'ssh -p %s %s %s@%s "[ -d %s ] && echo true"',
-                [
-                    $port,
-                    $sshOptions,
-                    $user,
-                    $host,
-                    $remoteDataTranslationsPath]
-            )
-        );
-
-        if ($translationsAvailable === 'true') {
-            $this->executeLocalShellCommand(
-                'rsync -e "ssh -p %s %s" -kLr %s@%s:%s/* %s',
-                [
-                    $port,
-                    addslashes($sshOptions),
-                    $user,
-                    $host,
-                    $remoteDataTranslationsPath,
-                    $localDataTranslationsPath
-                ]
-            );
-        }
 
         ################
         # Clear Caches #
@@ -372,7 +195,7 @@ class CloneCommandController extends AbstractCommandController
         ##################
         # Set DB charset #
         ##################
-        if ($this->databaseConfiguration['driver'] == 'pdo_mysql' && $remotePersistenceConfiguration['charset'] != 'utf8mb4' ) {
+        if ($this->databaseConfiguration['driver'] == 'pdo_mysql') {
             $this->renderHeadLine('Set DB charset');
             $this->executeLocalFlowCommand('database:setcharset');
         }
@@ -417,29 +240,5 @@ class CloneCommandController extends AbstractCommandController
         $this->renderLine('Successfully cloned in %s seconds', [$duration]);
     }
 
-    /**
-     * @param $remotePersistenceConfiguration
-     * @param $this ->databaseConfiguration
-     * @throws \Neos\Flow\Mvc\Exception\StopActionException
-     */
-    protected function checkConfiguration($remotePersistenceConfiguration)
-    {
-        $this->renderHeadLine('Check Configuration');
-        if (!$this->dbal->driverIsSupported($remotePersistenceConfiguration['driver'])
-            && !$this->dbal->driverIsSupported($this->databaseConfiguration['driver'])) {
-            $this->renderLine(sprintf('<error>ERROR:</error> Only pdo_pgsql and pdo_mysql drivers are supported! Remote: "%s" Local: "%s" configured.', $remotePersistenceConfiguration['driver'], $this->databaseConfiguration['driver']));
-            $this->quit(1);
-        }
-        if ($remotePersistenceConfiguration['driver'] !== $this->databaseConfiguration['driver']) {
-            $this->renderLine('<error>ERROR:</error> Remote and local databases must use the same driver!');
-            $this->quit(1);
-        }
-        if (in_array($remotePersistenceConfiguration['charset'], ['utf8', 'utf8mb4']) && in_array($this->databaseConfiguration['charset'], ['utf8', 'utf8mb4'])) {
-            // we accept utf8 and utf8mb4 being similar enough
-        } else if ($remotePersistenceConfiguration['charset'] != $this->databaseConfiguration['charset']) {
-            $this->renderLine(sprintf('<error>ERROR:</error> Remote and local databases must use the same charset! Remote: "%s", Local: "%s" configured.', $remotePersistenceConfiguration['charset'], $this->databaseConfiguration['charset']));
-            $this->quit(1);
-        }
-        $this->renderLine(' - Configuration seems ok ...');
-    }
+
 }
---
+++ Classes/Service/LambdaService.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Sitegeist\MagicWand\Service;
+
+use Neos\Flow\Annotations as Flow;
+use Aws\Lambda\LambdaClient;
+use Aws\S3\S3Client;
+use GuzzleHttp\Psr7\InflateStream;
+
+class LambdaService
+{
+
+    /**
+     * @Flow\InjectConfiguration(package="Sitegeist.MagicWand", path="aws")
+     * @var array
+     */
+    protected $settings;
+
+    public function getLambdaContent() : string
+    {
+        $config = [
+            'profile' => 'default',
+            'version' => 'latest',
+            'region'  => $this->settings['region'],
+            'http'    => [
+                'timeout' => 0,
+                'connect_timeout' => 0
+            ]
+        ];
+
+        $result = $this->executeLambda($config);
+        $file = $this->fetchDumpFromS3($result, $config);
+        return $this->writeFileToData($file);
+    }
+
+    /**
+     * @param \Aws\Result $result
+     * @param array $config
+     * @return \Aws\Result
+     */
+    private function fetchDumpFromS3(\Aws\Result $result, array $config): \Aws\Result
+    {
+        $fileToDownload = json_decode((string)$result->get('Payload'), true);
+        $s3 = new S3Client($config);
+        $file = $s3->getObject($fileToDownload);
+        return $file;
+    }
+
+    /**
+     * @param \Aws\Result $file
+     */
+    private function writeFileToData(\Aws\Result $file): string
+    {
+        $filename = FLOW_PATH_DATA . 'dump.sql';
+        $inflatedBody = new InflateStream($file['Body']); // This is now readable
+        $file = fopen($filename, 'a');
+        fwrite($file, (string)$inflatedBody);
+        fclose($file);
+        return $filename;
+    }
+
+    /**
+     * @param array $config
+     * @return \Aws\Result
+     */
+    private function executeLambda(array $config): \Aws\Result
+    {
+        $client = new LambdaClient($config);
+        $result = $client->invoke([
+            'FunctionName' => $this->settings['functionName'],
+            'Timeout' => 0
+        ]);
+        return $result;
+    }
+
+}
